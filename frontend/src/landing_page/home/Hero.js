13bb1e87e3c3e45a50ee4dcbf3ca7725a8a06d23
07rathore <ayush5677rathour@gmail.com>
2026-06-24 13:40:21 +0530

    Make home page responsive on mobile, fix dead signup buttons

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
+    const navigate = useNavigate();
     return ( 
         <div className="container p-5 mb-5">
         <div className='row text-center'>
-            <img src= "Media/homeHero.png" alt="Hero img" className="mb-5" />
+            {/* <img src= "Media/homeHero.png" alt="Hero img" className="mb-5" /> */}
+            <img src="/Media/homeHero.png" alt="Hero img" className="mb-5 img-fluid" />
             <h1 className='mt-5'>
              Invest in your future with our platform
             </h1>
             <p>
             Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more
             </p>
-               <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"25%",margin:"0 auto"}}>Signup Now</button> 
+               <button className='p-2 btn btn-primary fs-5 mb-5' onClick={() => navigate('/signup')} style={{width:"100px",margin:"0 auto"}}>Signup Now</button> 
             </div>
         </div>
         
--- frontend/src/landing_page/home/pricing.js
+++ frontend/src/landing_page/home/pricing.js
@@ -1,40 +1,39 @@
 import React from 'react';
 
 function Pricing() {
-    return ( 
-        <div className = 'container mb-5'>
-         <div className = 'row'>
-            <div className = 'col-5'>
-                <h1 className = 'mb-3 fs-2'>Unbeatable pricing</h1>
-                <p>We pioneered the concept of discount broking and price transparency in India. 
-                    Flat fees and no hidden charges.</p>
-            <a href='#' style = {{textDecoration:"none"}}>See pricing  <i class="fa-solid fa-arrow-right"></i></a> 
-            </div>
-            {/* <div className = 'col-2'></div> */}
-            <div className = 'col-7'>
-                <div className = 'row'>
-                    <div className = 'col' style={{display:"inline-flex"}}>
-                          <img src = "Media/pricing0.svg" style={{width:"50%"}} />
-                            <p className ='text-muted  mb-15' style={{marginTop:"50px",fontSize:"0.75rem"}}>Free account<br></br>opening</p>
+    return (
+        <div className='container mb-5'>
+            <div className='row'>
+                <div className='col-12 col-md-5'>
+                    <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India.
+                        Flat fees and no hidden charges.</p>
+                    <a href='#' style={{ textDecoration: "none" }}>See pricing  <i className="fa-solid fa-arrow-right"></i></a>
+                </div>
+                <div className='col-12 col-md-7 mt-3'>
+                    <div className='row'>
+                        <div className='col-12 col-sm-4 text-center mb-3'>
+                            <img src="/Media/pricing0.svg" alt="Free account opening" style={{ width: "40%" }} />
+                            <p className='text-muted mt-2' style={{ fontSize: "0.75rem" }}>Free account<br></br>opening</p>
+                        </div>
+                        <div className='col-12 col-sm-4 text-center mb-3'>
+                            <img src="/Media/pricing0.svg" alt="Free equity delivery" style={{ width: "40%" }} />
+                            <p className='text-muted mt-2' style={{ fontSize: "0.75rem" }}>Free equity delivery<br></br>and direct mutual fund</p>
+                        </div>
+                        <div className='col-12 col-sm-4 text-center mb-3'>
+                            <img src="/Media/intradayTrades.svg" alt="Intraday and F&O" style={{ width: "40%" }} />
+                            <p className='text-muted mt-2' style={{ fontSize: "0.75rem" }}>Intraday and<br></br>F&O</p>
+                        </div>
+
+
                     </div>
-                     <div className = 'col' style={{display:"inline-flex"}}>
-                          <img src = "Media/pricing0.svg" style={{width:"50%"}} />
-                            <p className ='text-muted  mb-15' style={{marginTop:"30px",fontSize:"0.75rem"}}>Free equity delivery<br></br>and direct mutual fund</p>
-                    </div>
-                     <div className = 'col mb-5' style={{display:"inline-flex"}}>
-                          <img src = "Media/intradayTrades.svg" style={{width:"50%"}} />
-                            <p className ='text-muted  mb-15' style={{marginTop:"20px",fontSize:"0.75rem"}}>Intraday and<br></br>F&O</p>
-                    </div>
-                  
-                    
                 </div>
             </div>
-            </div>   
 
         </div>
-     );
-                            
-    
+    );
+
+
 }
 
 export default Pricing;
